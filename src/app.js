eb19d30b8d8f8bb92751b77c740201166105e393
Helton Vasconcelos <heltonvasconcelos88@gmail.com>
2019-09-18 23:01:53 -0300

    serving templates and partials with express

--- src/app.js
+++ src/app.js
@@ -1,17 +1,32 @@
 const path = require('path');
-
+const hbs = require('hbs');
 const express = require('express');
 
 const app = express();
 
+// Define path for express config
 const publicDirectoryPath = path.join(__dirname, '../public');
-console.log(publicDirectoryPath);
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
+// Setup handlebars engine and view location
 app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static dir to serve
 app.use(express.static(publicDirectoryPath));
 
 app.get('', (req, res) => {
-  res.send('Hi, =)!');
+  res.render('index', { title: 'Weather', name: 'Helton' });
+});
+
+app.get('/help', (req, res) => {
+  res.render('help', { title: 'Help', name: 'Helton', helpText: 'Help Text.' });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About', name: 'Helton' });
 });
 
 app.get('/weather', (req, res) => {
